fc65a7afe1a11422e4cf9009c0b08ccdf87f1aa8
MateusCapitanio <mateusjmj92@gmail.com>
2023-03-14 11:32:52 -0300

    feat: Subindo versão com URL socket nas envs

--- context/WebSocketContext.tsx
+++ context/WebSocketContext.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { io, Socket } from 'socket.io-client';
 
-export const socket = io('https://coinlivre.blocklize.io/')
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!)
 
 export const WebSocketContext = React.createContext<Socket>(socket)
 
